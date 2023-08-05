bdbef369ab804d5aad30ec2de4966ece64515387
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-08-05 19:42:37 +0400

    Добавьте файлы проекта.

---
+++ HappyTickets/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTickets
+{
+    internal class Program
+    {
+        static string HappyTicket(string ticket) 
+        {
+            int ticket1 = Int32.Parse(ticket);
+            int ticket0 = ticket1 - 1;
+            int a = ticket0 % 10;
+            int b = ticket0 / 10 % 10;
+            int c = ticket0 / 100 % 10;
+            int d = ticket0 / 1000 % 10;
+            int e = ticket0 / 10000 % 10;
+            int f = ticket0 / 100000;
+            int g = a + b + c;
+            int h = f + d + e;
+            int i = g - h;
+            int j = h - g;
+            if ((i == 1) || (j == 1))
+            {
+                return "This ticket is happy";
+            }
+            else
+            {
+                return "This ticket is unhappy";
+            }
+        }
+        static string HappyTicket0(string ticket)
+        {
+            int ticket1 = Int32.Parse(ticket);
+            int ticket0 = ticket1 + 1;
+            int a1 = ticket0 % 10;
+            int b1 = ticket0 / 10 % 10;
+            int c1 = ticket0 / 100 % 10 ;
+            int d1 = ticket0 / 1000 % 10;
+            int e1 = ticket0 / 10000 % 10;
+            int f1 = ticket0 / 100000;
+            int g1 = a1 + b1 + c1;
+            int h1 = f1 + d1 + e1;
+            int i1 = g1 - h1;
+            int j1 = h1 - g1;
+            if ((i1 == 1) || (j1 == 1))
+            {
+                return "This ticket is happy";
+            }
+            else
+            {
+                return "This ticket is unhappy";
+            }
+        }
+        static void Main(string[] args)
+        {
+            string ticket = HappyTicket("666664");
+            string ticket0 = HappyTicket0("666664");
+            Console.WriteLine(ticket);
+            Console.WriteLine(ticket0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HappyTickets/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HappyTickets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HappyTickets")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a4209a14-2b33-4da8-90f7-6d71746e9da3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
